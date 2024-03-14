ccb0f06105fb7f0c217d1cf30697892c07e5a36c
Narendiran R <97464705+NARENDIR@users.noreply.github.com>
2024-03-14 22:33:37 +0530

    Update router_configuration.tf

--- router_configuration.tf
+++ router_configuration.tf
@@ -1,43 +1,41 @@
 # router_configuration.tf
 
-provider aws {
-  region = your_region
+provider "aws" {
+  region = "us-west-2"  
 }
 
-resource aws_instance router {
-  ami           = your_router_ami
-  instance_type = t2.micro
-  key_name      = your_key_pair
+resource "aws_instance" "router" {
+  ami           = "ami-1234567890abcdef0"
+  instance_type = "t2.micro"
+  key_name      = "my-key-pair"
 
   # Other instance configuration...
 
   tags = {
-    Name = Router
+    Name = "Router"
   }
 }
 
-# Configure OSPF
-resource aws_network_interface router_interface {
+resource "aws_network_interface" "router_interface" {
   # Interface configuration...
 
   tags = {
-    Name = Router Interface
+    Name = "Router Interface"
   }
 }
 
-resource aws_route_table router_route_table {
+resource "aws_route_table" "router_route_table" {
   # Route table configuration...
 
   tags = {
-    Name = Router Route Table
+    Name = "Router Route Table"
   }
 }
 
-resource aws_route ospf_route {
-  route_table_id         = aws_route_table.router_route_table.id
-  destination_cidr_block = 0.0.0.00
+resource "aws_route" "ospf_route" {
+  route_table_id         = "rtb-0123456789abcdef0"  
+  destination_cidr_block = "0.0.0.0/0"
   network_interface_id   = aws_network_interface.router_interface.id
-  instance_id            = aws_instance.router.id
+  instance_id            = aws_instance.router.id 
 }
 
-# Additional configuration for BGP if needed...
